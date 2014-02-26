a36dd873a213b2cc61a90a5ead694660f5c589d6
oneoo <oneoo@yo2.com>
2014-02-26 14:16:09 +0800

    reformat smp debug logs

--- common/smp.c
+++ common/smp.c
@@ -1,6 +1,19 @@
 #include "smp.h"
 #include "log.h"
 
+extern int is_daemon;
+extern int process_count;
+extern int pid;
+
+extern time_t now;
+extern struct tm _now_gtm;
+extern struct tm _now_lc;
+extern char now_gmt[32];
+extern char now_lc[32];
+
+extern logf_t *LOGF_T;
+extern int LOG_LEVEL;
+
 /*
 simple memory pool
 */
